a995db9b9a70fadc9f8cba7626b89c842de6857f
Austin Brown <austinsbrown@gmail.com>
2021-01-05 15:43:39 -0600

    added more connections

--- datapath.v
+++ datapath.v
@@ -3,6 +3,9 @@
 `include "control.v"
 `include "registers.v"
 `include "mux.v"
+`include "sign_extend.v"
+`include "alu_control.v"
+`include "alu.v"
 
 module datapath
 (
@@ -15,16 +18,17 @@ module datapath
     wire [31:0] signExtend;                                                                     // output of signExtend module
     wire [31:0] instruction_address;                                                            // output of the program counter
     wire [31:0] instruction;                                                                    // the instruction to be executed
-    wire [31:0] readReg1, readReg2;
+    wire [31:0] readData1, readData2;
     wire [31:0] aluOut;                                                                         // result of the alu
     wire [31:0] readData;                                                                       // result of reading data memory
     wire [4:0]  regMux;
     wire [31:0] aluMux, memMux, pcIn;
     wire [31:0] shiftLeftAdder, SimpleAdder;
     wire andOut;
+    wire [3:0] aluCtrl;
 
     mux #(5)  intoReg(regDst, instruction[20:16], instruction[15:11], regMux);                  // multiplexer that feeds into the regesters
-    mux #(32) intoALU(aluSrc, readReg2, signExtend, aluMux);                                    // multiplexer that feeds into the alu
+    mux #(32) intoALU(aluSrc, readData2, signExtend, aluMux);                                   // multiplexer that feeds into the alu
     mux #(32) outMem(memToReg, aluOut, readData, memMux);                                       // multiplexer that takes output from alu and mem 
     mux #(32) pcMux(andOut, SimpleAdder, shiftLeftAdder, pcIn);                                 // mux that feeds into the program counter
 
@@ -42,4 +46,20 @@ module datapath
         regWrite,
         memToReg
     );
+
+    registers regs
+    (
+        clk,
+        regWrite,
+        instruction[25:21],
+        instruction[20:16],
+        regMux,
+        memMux,
+        readData1,
+        readData2
+    );
+
+    sign_extend se(instruction[15:0], signExtend);
+    alu_control aluC(aluOP, instruction[5:0], aluCtrl);
+    alu aluModule(readData1, aluMux, aluCtrl, aluOut, andOut);
 endmodule
