3f4d3c0b78e30fc6eee1c1e58e609c1c5f86f80e
Austin Brown <austinsbrown@gmail.com>
2021-01-05 09:25:36 -0600

    added multiplexers to datapath

--- datapath.v
+++ datapath.v
@@ -2,6 +2,7 @@
 `include "instruction_memory.v"
 `include "control.v"
 `include "registers.v"
+`include "mux.v"
 
 module datapath
 (
@@ -9,11 +10,23 @@ module datapath
     input clk
 );
 
-    wire regDst, branch, memRead, memToReg, memWrite, aluSrc, regWrite;
-    wire [1:0]  aluOP;
-    wire [31:0] signExtend;
-    wire [31:0] instruction_address;
-    wire [31:0] instruction;
+    wire regDst, branch, memRead, memToReg, memWrite, aluSrc, regWrite;                         // control wires
+    wire [1:0]  aluOP;                                                                          
+    wire [31:0] signExtend;                                                                     // output of signExtend module
+    wire [31:0] instruction_address;                                                            // output of the program counter
+    wire [31:0] instruction;                                                                    // the instruction to be executed
+    wire [31:0] readReg1, readReg2;
+    wire [31:0] aluOut;                                                                         // result of the alu
+    wire [31:0] readData;                                                                       // result of reading data memory
+    wire [4:0]  regMux;
+    wire [31:0] aluMux, memMux, pcIn;
+    wire [31:0] shiftLeftAdder, SimpleAdder;
+    wire andOut;
+
+    mux #(5)  intoReg(regDst, instruction[20:16], instruction[15:11], regMux);                  // multiplexer that feeds into the regesters
+    mux #(32) intoALU(aluSrc, readReg2, signExtend, aluMux);                                    // multiplexer that feeds into the alu
+    mux #(32) outMem(memToReg, aluOut, readData, memMux);                                       // multiplexer that takes output from alu and mem 
+    mux #(32) pcMux(andOut, SimpleAdder, shiftLeftAdder, pcIn);                                 // mux that feeds into the program counter
 
     program_counter pc(clk, reset, branch, signExtend, instruction_address);
     instruction_memory iMem(instruction_address[7:0], instruction);
@@ -29,13 +42,4 @@ module datapath
         regWrite,
         memToReg
     );
-
-    registers regs
-    (
-        clk,
-        regWrite,
-        instruction[25:21],
-        instruction[20:16],
-    );
-
 endmodule
