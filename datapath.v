039977fe5b4c63e4187a07c282d8f2b72a1566b1
Austin Brown <austinsbrown@gmail.com>
2021-01-08 10:32:50 -0600

    added inputs and outputs

--- datapath.v
+++ datapath.v
@@ -5,7 +5,17 @@
 
 module datapath
 (
-    
+    input clk,
+    input reset,
+    input RegDst,
+    input AluSrc,
+    input MemtoReg,
+    input RegWrite,
+    input MemRead,
+    input MemWrite,
+    input Branch,
+    input [1:0] ALUOp,
+    output [5:0] OpCode
 );
 
     
