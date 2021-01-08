13d7015f1e8d0f9cc0c1223c76e9fe42fe9fca6b
Austin Brown <austinsbrown@gmail.com>
2021-01-08 10:51:20 -0600

    added several wires

--- datapath.v
+++ datapath.v
@@ -18,6 +18,10 @@ module datapath
     output [5:0] OpCode
 );
 
-    
+    wire [31:0] instruction                                                         // the instruction read from instruction memory
+    wire [31:0] PCIn;                                                               // input for the pc 
+    wire [7:0] PCOut;                                                               // input for instruction memory
+    wire [31:0] operand1, operand2;                                                 // inputs for the alu
+    wire [3:0]  ALUControl;
 
 endmodule
