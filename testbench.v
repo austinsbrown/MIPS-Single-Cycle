198fab169e3287d1295cf83e2d779f6c6351e1a8
Austin Brown <austinsbrown@gmail.com>
2021-01-08 11:02:13 -0600

    fixed the program counter (maybe)

--- testbench.v
+++ testbench.v
@@ -1,55 +1,33 @@
 `timescale 1s/100ns
-`include "alu.v"
+`include "program_counter.v"
 
 module testbench();
-    
-    reg [31:0] operand1;
-    reg [31:0] operand2;
-    reg [3:0]  aluOP;
-    wire[31:0] result;
-    wire zeroFlag;     
+    reg clk, reset;
+    reg [31:0] pcIn;
+    wire [31:0] pcOut;
 
-    alu alu_uut(operand1, operand2, aluOP, result, zeroFlag);
+    program_counter pc(clk, reset, pcIn, pcOut);
 
     initial begin
         $dumpfile("dump.vcd");
         $dumpvars(0, testbench);
 
-        operand1 = 3;
-        operand2 = 2;
-        aluOP = 4'b0010;
+        clk = 0;
+        reset = 0;
+        pcIn = 0;
+        #100;
+
+        reset = 1;
         #10;
 
-        operand1 = 3;
-        operand2 = 3;
-        aluOP = 4'b0110;
+        reset = 0;
+        pcIn = 20;
         #10;
-
-        operand1 = 4'b1111;
-        operand2 = 4'b0000;
-        aluOP = 4'b0000;
-        #10;
-
-        operand1 = 4'b1111;
-        operand2 = 4'b0000;
-        aluOP = 4'b0001;
-        #10;
-
-        operand1 = 4'b1111;
-        operand2 = 4'b0000;
-        aluOP = 4'b0111;
-        #10;
-
-        operand1 = 4'b1111;
-        operand2 = 4'b0000;
-        aluOP = 4'b0110;
-        #10;
-
     end
 
-    /*always
+    always
     #5  clk = ! clk; 
 
     initial 
-        #150  $finish; */
+        #150  $finish; 
 endmodule
